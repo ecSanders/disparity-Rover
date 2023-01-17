692825b5b88c523b9a763f310a6488185d708930
ecSanders <ecsander0@gmail.com>
2023-01-17 11:25:45 -0700

    Add stub functions

---
+++ vSLAM/map_initialization.py
@@ -0,0 +1,39 @@
+import cv2
+"""
+Title: map_initalization.py
+Authors: Erik Sanders, Jared Perlic, Tyson Mergle
+Date Start: Jan 16, 2023
+Description:
+
+This file is meant for the first part of the vSLAM algorithm. These steps
+are as follows:
+
+- Detect
+- Extract
+- Match
+- Estimate relative camera pose from matched feature points
+- Find 3-D locations of the matched feature points
+- Refine initial map
+- Manage data for initial map and key frames
+"""
+
+def detect():
+    pass
+
+def extract():
+    pass
+
+def match():
+    pass
+
+def estimate_pose():
+    pass
+
+def project_points():
+    pass
+
+def refine_map():
+    pass
+
+def manage_mapframes():
+    pass
---
+++ vSLAM/track.py
@@ -0,0 +1,37 @@
+"""
+Title: track.py
+Authors: Erik Sanders, Jared Perlic, Tyson Mergle
+Date Start: Jan 16, 2023
+Description:
+
+This file is meant for the first part of the vSLAM algorithm. These steps
+are as follows:
+
+- Match extracted features
+- Estimate camera pose
+- Project map points
+- Search for feature correspondences
+- Refine camera pose
+- Identify local map points
+- Search for more feature correspondences
+- Refine camera pose 
+- Store new key frame
+"""
+
+def match_frames():
+    pass
+
+def estimate_pose():
+    pass
+
+def correspespond():
+    pass
+
+def refine_pose():
+    pass
+
+def id_map_points():
+    pass
+
+def store_frames():
+    pass
