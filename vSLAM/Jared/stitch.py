fde3e90075fbd1ee6bd1e8a5184f100c0091c9bf
Jared Perlic <jperlic@byui.edu>
2023-01-21 15:10:09 -0700

    Added stitch_old.py and stitch.py

---
+++ vSLAM/Jared/stitch.py
@@ -0,0 +1,20 @@
+import stitching
+import time
+
+img_path = '../../Images'
+
+# Create a Stitcher
+settings = {"detector": "sift", "confidence_threshold": 0.2}
+stitcher = stitching.Stitcher(**settings)
+
+# Start the timer
+start = time.time()
+
+# Perform the stitch
+res = stitcher.stitch(['{img_path}/stitch01_small.jpg', '{img_path}/stitch02_small.jpg'])
+
+# Stop the timer
+stop = time.time()
+
+# Print the time taken to stitch
+print(f'Time to stitch: {round(stop - start, 2)}s')
