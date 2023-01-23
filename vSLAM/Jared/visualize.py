6b277c2f2e46c8f26d208e79120e203202ecf6d9
Jared Perlic <jperlic@byui.edu>
2023-01-23 15:18:06 -0700

    Added data.ply

--- vSLAM/Jared/visualize.py
+++ vSLAM/Jared/visualize.py
@@ -7,4 +7,4 @@ arr = np.random.rand(100, 3)
 # Pass array to Open3D.o3d.geometry.PointCloud and visualize
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(arr)
-o3d.io.write_point_cloud("./data.ply", pcd)
+o3d.io.write_point_cloud('data.ply', pcd)
