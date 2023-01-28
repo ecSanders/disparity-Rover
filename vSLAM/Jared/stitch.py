d65abad598f797383a86b06809323cd402240f81
Jared Perlic <jperlic@byui.edu>
2023-01-28 12:51:41 -0700

    Added match.py

--- vSLAM/Jared/stitch.py
+++ vSLAM/Jared/stitch.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import imutils
 import time
 
 # Get file names
@@ -9,12 +8,12 @@ files = 'stitch01_small.jpg stitch02_small.jpg'  # Small images aren't stitched 
 # Load images into list
 imgs = [cv.imread(f'../../Images/{file}') for file in files.split(' ')]
 
-# # View images
-# for img in imgs:
-#     cv.imshow(f'Image', img)
+# View images
+for img in imgs:
+    cv.imshow(f'Image', img)
 
-#     cv.waitKey(0)
-#     cv.destroyAllWindows()
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 # Create stitching object
 stitcher = cv.Stitcher.create(cv.Stitcher_SCANS)
@@ -32,9 +31,9 @@ stop = time.time()
 # Make sure it was successful
 assert status == 0
 
+# Print the time taken to stitch
+print(f'Time to stitch: {round(stop - start, 2)}s')
+
 cv.imshow('Result', res)
 cv.waitKey(0)
 cv.destroyAllWindows()
-
-# Print the time taken to stitch
-print(f'Time to stitch: {round(stop - start, 2)}s')
