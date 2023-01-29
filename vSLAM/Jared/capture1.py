874cc479493ea18ecd9576dc2d07e31fcb5de1bf
Jared Perlic <jperlic@byui.edu>
2023-01-29 16:45:24 -0700

    Added prototype stereo capture, added distance.py

--- vSLAM/Jared/capture1.py
+++ vSLAM/Jared/capture1.py
@@ -1,21 +1,21 @@
 import cv2 as cv
 
 # Define a VideoCapture object
-vid = cv.VideoCapture(0)
+cam = cv.VideoCapture(0)
 
 while True:
 
     # Capture video frame by frame
-    ret, frame = vid.read()
+    ret, frame = cam.read()
 
-    # Display the frame
-    cv.imshow('frame', frame)
+    # Display the video
+    cv.imshow('disp', frame)
 
     # Press `q` to close the window
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release the VideoCapture object
-vid.release()
+cam.release()
 
 cv.destroyAllWindows()
--- vSLAM/Jared/match.py
+++ vSLAM/Jared/match.py
@@ -23,11 +23,14 @@ start = time.time()
 # Match descriptors
 matches = bf.match(des1, des2)
 
+# Make sure at least 15 feature matches were found
+assert len(matches) >= 15
+
 # Sort them in the order of their distance
-matches = sorted(matches, key=lambda x: x.distance)
+matches = sorted(matches, key=lambda x:x.distance)
 
 # Draw first 10 matches
-img3 = cv.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+img3 = cv.drawMatches(img1, kp1, img2, kp2, matches[:15], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
 # Stop the timer
 stop = time.time()
