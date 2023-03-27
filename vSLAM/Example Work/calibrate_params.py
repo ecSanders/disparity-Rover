28a162d598ae320f1ec1e7abe93d00890d84af0a
Erik Sanders <ecsander0@gmail.com>
2023-03-27 15:16:36 -0600

    Random push

--- vSLAM/Example Work/calibrate_params.py
+++ vSLAM/Example Work/calibrate_params.py
@@ -6,14 +6,19 @@ with open("calibrate01.pkl", 'rb') as f:  # open a text file
 
 with open("calibrate02.pkl", 'rb') as f:  # open a text file
     calibrate02 = pickle.load(f) # serialize the list
+
 width, height = 640,480
 
 print(calibrate01.keys())
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 stereocalibration_flags = cv2.CALIB_FIX_INTRINSIC            #                            RED:left                  BLACK:right
-ret, CM1, dist1, CM2, dist2, R, T, E, F = cv2.stereoCalibrate(calibrate01['objpoints'], calibrate01['imgpoints'], calibrate02['imgpoints'],
+retS, new_mtxL, distL, new_mtxR, distR, Rot, Trns, Emat, Fmat = cv2.stereoCalibrate(calibrate01['objpoints'], calibrate01['imgpoints'], calibrate02['imgpoints'],
                                                               calibrate01['camMat'], calibrate01['Dist coeffs'],
                                                               calibrate02['camMat'], calibrate02['Dist coeffs'], 
                                                               (width, height), criteria = criteria, flags = stereocalibration_flags)
 
-print(ret, CM1, dist1, CM2, dist2, R, T, E, F)
+# print(ret, CM1, dist1, CM2, dist2, R, T, E, F)
+
+
+rectify_scale= 1
+rect_l, rect_r, proj_mat_l, proj_mat_r, Q, roiL, roiR= cv2.stereoRectify(new_mtxL, distL, new_mtxR, distR, imgL_gray.shape[::-1], Rot, Trns, rectify_scale,(0,0))
--- vSLAM/Example Work/disparity.py
+++ vSLAM/Example Work/disparity.py
@@ -9,90 +9,69 @@ This script generates a disparity map from a stereo camera.
 import cv2 as cv
 import numpy as np
 
-CamL_id = 2  # Camera ID for left camera
-CamR_id = 0  # Camera ID for right camera
 
 # Define two VideoCapture objects
-CamL = cv.VideoCapture(CamL_id)
-CamR = cv.VideoCapture(CamR_id)
+Cam = cv.VideoCapture(1)
 
 # Read and map values for stereo image rectification
-cv_file = cv.FileStorage("stereo_rectify_maps.xml", cv.FILE_STORAGE_READ)
+cv_file = cv.FileStorage("data/params_py.xml", cv.FILE_STORAGE_READ)
 Left_Stereo_Map_x = cv_file.getNode("Left_Stereo_Map_x").mat()
 Left_Stereo_Map_y = cv_file.getNode("Left_Stereo_Map_y").mat()
 Right_Stereo_Map_x = cv_file.getNode("Right_Stereo_Map_x").mat()
 Right_Stereo_Map_y = cv_file.getNode("Right_Stereo_Map_y").mat()
 cv_file.release()
- 
-# Reading the stored StereoBM parameters
-cv_file = cv.FileStorage("disparity_params.xml", cv.FILE_STORAGE_READ)
-numDisparities = int(cv_file.getNode("numDisparities").real())
-blockSize = int(cv_file.getNode("blockSize").real())
-preFilterType = int(cv_file.getNode("preFilterType").real())
-preFilterSize = int(cv_file.getNode("preFilterSize").real())
-preFilterCap = int(cv_file.getNode("preFilterCap").real())
-textureThreshold = int(cv_file.getNode("textureThreshold").real())
-uniquenessRatio = int(cv_file.getNode("uniquenessRatio").real())
-speckleRange = int(cv_file.getNode("speckleRange").real())
-speckleWindowSize = int(cv_file.getNode("speckleWindowSize").real())
-disp12MaxDiff = int(cv_file.getNode("disp12MaxDiff").real())
-minDisparity = int(cv_file.getNode("minDisparity").real())
-cv_file.release()
- 
+
 # Create an object of StereoBM algorithm
-stereo = cv.StereoBM_create()
+
+minDisparity=30
+numDisparities=160
+stereo = cv.StereoBM_create(numDisparities=numDisparities, blockSize=5)
 
 while True:
 
     # Capture and store left and right camera images
-    retL, imgL = CamL.read()
-    retR, imgR = CamR.read()
+    ret, img = Cam.read()
 
     # Proceed only if the frames have been captured
-    if retL and retR:
-        imgR_gray = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
-        imgL_gray = cv.cvtColor(imgL, cv.COLOR_BGR2GRAY)
+    if ret:
+        img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        # This shape is good
+        imgL_gray = img_gray[0:960, 1280:2560]
+        imgR_gray = img_gray[0:960, 0:1280]
 
         # Apply stereo image rectification on the left image
         Left_nice = cv.remap(imgL_gray,
                              Left_Stereo_Map_x,
                              Left_Stereo_Map_y,
-                             cv.INTER_LANCZOS4,
-                             cv.BORDER_CONSTANT,
-                             0)
-        
+                             cv.INTER_LINEAR)
+                            #  cv.BORDER_CONSTANT,
+                            #  0)
+
+
         # Apply stereo image rectification on the right image
         Right_nice = cv.remap(imgR_gray,
                               Right_Stereo_Map_x,
                               Right_Stereo_Map_y,
-                              cv.INTER_LANCZOS4,
-                              cv.BORDER_CONSTANT,
-                              0)
-        
-        # Set the updated parameters before computing disparity map
-        stereo.setNumDisparities(numDisparities)
-        stereo.setBlockSize(blockSize)
-        stereo.setPreFilterSize(preFilterSize)
-        stereo.setPreFilterCap(preFilterCap)
-        stereo.setTextureThreshold(textureThreshold)
-        stereo.setUniquenessRatio(uniquenessRatio)
-        stereo.setSpeckleRange(speckleRange)
-        stereo.setSpeckleWindowSize(speckleWindowSize)
-        stereo.setDisp12MaxDiff(disp12MaxDiff)
-        stereo.setMinDisparity(minDisparity)
+                              cv.INTER_LINEAR)
+                            #   cv.BORDER_CONSTANT,
+                            #   0)
+
+        cv.imshow("Left", Left_nice)
+        cv.imshow("Right", Right_nice)
 
         # Calculate disparity using the StereoBM algorithm
         disparity = stereo.compute(Left_nice, Right_nice)
         # NOTE: Code returns a 16-bit signed single channel image,
-        # CV_16S containing a disparity map scaled by 16. Hence it 
+        # CV_16S containing a disparity map scaled by 16. Hence it
         # is essential to convert it to CV_32F and scale it down 16 times.
-    
-        # Convert to float32 
+
+        # Convert to float32
         disparity = disparity.astype(np.float32)
-    
-        # Scale down the disparity values and normalizing them 
+
+        # Scale down the disparity values and normalizing them
         disparity = (disparity / 16.0 - minDisparity) / numDisparities
-    
+
         # Display the disparity map
         cv.imshow("disp", disparity)
 
@@ -101,11 +80,9 @@ while True:
             break
 
     else:
-        CamL = cv.VideoCapture(CamL_id)
-        CamR = cv.VideoCapture(CamR_id)
+        Cam = cv.VideoCapture(1)
 
 # Release the VideoCapture objects
-CamL.release()
-CamR.release()
+Cam.release()
 
 cv.destroyAllWindows()
