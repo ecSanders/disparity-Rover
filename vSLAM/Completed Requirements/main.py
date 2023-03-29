5460cd67d6d4078f02581c3454083727d37ebd29
Jared Perlic <jperlic@byui.edu>
2023-03-29 15:25:54 -0600

    Small fixes to main

--- vSLAM/Completed Requirements/main.py
+++ vSLAM/Completed Requirements/main.py
@@ -1,23 +1,27 @@
-# https://www.loom.com/share/eee8d4c2231943e8a54d2109c1c16b6f
+""" Loom link: https://www.loom.com/share/eee8d4c2231943e8a54d2109c1c16b6f """
+
+# Load requirements
 try:
     import os
     import cv2
     import numpy as np
     from obj_detection import detect
+
 except:
     dir_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(dir_path)
     os.system("pip install -r requirements.txt")
 
+
 def main(cam, stereo):
+
     # Initiate ORB detector
     orb = cv2.ORB_create()
 
     while True:
-        ### FRAME 01 ###
 
         # Get frames
-        retL, frame = cam.read()
+        ret, frame = cam.read()
 
         left = frame[0:960, 1280:2560]
         right = frame[0:960, 0:1280]
@@ -32,24 +36,24 @@ def main(cam, stereo):
         # Match descriptors
         matches = bf.match(des1, des2)
 
-        # Make sure at least 15 feature matches were found
-        assert len(matches) >= 5
+        # Make sure at least 10 feature matches were found
+        assert len(matches) >= 10
 
         # Sort them in the order of their distance
         matches = sorted(matches, key=lambda x:x.distance)
 
         # Draw first 10 matches
-        img3 = cv2.drawMatches(left, kp1, right, kp2, matches[:5], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        img3 = cv2.drawMatches(left, kp1, right, kp2, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
         # Detect objects (COCO images)
         left_det = detect(img3)
 
-        #Resize to view
+        # Resize to view
         sized = cv2.resize(left_det, (1280, 480))
 
+        # Display camera feed
         cv2.imshow("Camera Feed", sized)
 
-        ### FRAME 02 ###
         img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         imgL_gray = img_gray[0:960, 1280:2560]
@@ -79,7 +83,7 @@ def main(cam, stereo):
         # Convert to float32
         disparity = disparity.astype(np.float32)
 
-        # Scale down the disparity values and normalizing them
+        # Scale down the disparity values and normalize them
         disparity = (disparity / 16.0 - minDisparity) / numDisparities
 
         # Display the disparity map
@@ -90,13 +94,11 @@ def main(cam, stereo):
             break
 
     cam.release()
-
     cv2.destroyAllWindows()
 
 
-
-
 if __name__ == "__main__":
+
     # Read and map values for stereo image rectification
     cv_file = cv2.FileStorage("data/params_py.xml", cv2.FILE_STORAGE_READ)
     Left_Stereo_Map_x = cv_file.getNode("Left_Stereo_Map_x").mat()
@@ -105,12 +107,10 @@ if __name__ == "__main__":
     Right_Stereo_Map_y = cv_file.getNode("Right_Stereo_Map_y").mat()
     cv_file.release()
 
-    # Create an object of StereoBM algorithm
-
-    minDisparity=30
-    numDisparities=160
+    minDisparity = 30
+    numDisparities = 160
     stereo = cv2.StereoBM_create(numDisparities=numDisparities, blockSize=5)
 
-    cam = cv2.VideoCapture(1)  # Camera ID for left camera
+    cam = cv2.VideoCapture(1)
 
     main(cam, stereo)
